86951336cc2217f0940757674a7412665fc6a97f
jWarrenDev <Jerrick.Warren@gmail.com>
2018-09-04 11:30:39 -0700

    modified pets

--- Pets/View Controller/AnimalList.swift
+++ Pets/View Controller/AnimalList.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class Animal {
 
-    let animalList = ["Happy the Monkey", "Chuck the Aligator", "Billy the Walrus","Frankie the Feret", "Barbie the pig", "Donald the Duck"]
+    let animalList = ["Happy the Monkey", "Chuck the Aligator", "Billy the Walrus","Frankie the Feret", "Barbie the pig", "Donald the Duck", "Silly the Salamander"]
 
 }
 
